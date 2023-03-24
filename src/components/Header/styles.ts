e31d9fb89ef2a3aa8587700429899e79fe1c935c
João Felipe N Santos <joaofnsantos@gmail.com>
2023-03-24 18:46:53 -0300

    :sparkles: Montado estrutura básica com Header & Footer

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  width: 100vw;
+  height: auto;
+  padding: 1rem 6.4rem;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  
+  background: #E2E9FA;
+`
